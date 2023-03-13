5f8b5a7ac3bcc854d4bd2a94e36106934fa9536d
Enzo Rafael <enzoricarteaa@gmail.com>
2023-03-13 13:56:59 -0300

    Status de Fome, Sede, Vida e Temperatura
    Uma cena de teste, com dois códigos

---
+++ Alone_TI_3_4/Assets/Scripts/Barra_Status.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Barra_Status : MonoBehaviour
+{
+     public Slider slider;
+      //Define o valor maximo
+     public void ColocarStatusMaxima(int statusMax){
+        slider.maxValue = statusMax;
+        slider.value = statusMax;
+     }
+     //Define o valor minimo
+     public void ColocarStatusMamino(int statusMin){
+        slider.minValue = statusMin;
+     }
+     public void AlterarStatus(int statusVal){       
+        slider.value = statusVal;  
+     }///Define o valor maximo da temperatura
+     public void ColocarTempMaxima(float statusMax){
+        slider.maxValue = statusMax;
+        slider.value = 0f;
+     }
+     //Define o valor minimo da temperatura
+     public void ColocarTempMinimo(float statusMin){
+        slider.minValue = statusMin;
+     }//Atera o valor usando um float
+     public void AlterarStatusFlo(float statusVal){
+       slider.value = statusVal;
+     }
+}
---
+++ Alone_TI_3_4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Barra_Status barraVida;
+    public Barra_Status barraFome;
+    public Barra_Status barraSede;
+    public Barra_Status barra_Temp;
+
+    public int vida = 100;
+    public int fome = 50;
+    public int sede = 50;
+    public float tempMin = -50.00f;
+    public float tempMax =  50.00f;
+
+    void Start()
+    {
+        vida = 100;
+        barraVida.ColocarStatusMaxima(vida);
+        fome = 50;
+        barraFome.ColocarStatusMaxima(fome);
+        sede = 50;
+        barraSede.ColocarStatusMaxima(sede);
+        tempMin = -50.00f;
+        tempMax =  50.00f;
+        barra_Temp.ColocarTempMaxima(tempMax);
+        barra_Temp.ColocarTempMinimo(tempMin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //estrutura exemplo de condição de dano e testes
+        if(Input.GetKeyDown(KeyCode.D)){
+            vida -= 10;
+            barraVida.AlterarStatus(vida);
+        }
+         if(Input.GetKeyDown(KeyCode.E)){
+            vida += 10;
+            barraVida.AlterarStatus(vida);
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            fome -= 10;
+            barraFome.AlterarStatus(fome);
+        }
+        if(Input.GetKeyDown(KeyCode.Q)){
+            fome += 10;
+            barraFome.AlterarStatus(fome);
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            sede -= 10;
+            barraSede.AlterarStatus(sede);
+        }
+        if(Input.GetKeyDown(KeyCode.W)){
+            sede += 10;
+            barraSede.AlterarStatus(sede);
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            tempMax += 10f;
+            barra_Temp.AlterarStatusFlo(tempMax);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            tempMax -= 10f;
+            barra_Temp.AlterarStatusFlo(tempMax);
+        }
+    }
+}
