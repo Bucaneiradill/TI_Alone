b1a208181fda19a50f90e074882d31e60fbbfca1
Enzo Rafael <enzoricarteaa@gmail.com>
2023-04-23 12:24:43 -0300

    Update cod Hud, Velocidade do dia, correções no padrão de linguagem do texto

--- Alone_TI_3_4/Assets/Scripts/Hud.cs
+++ Alone_TI_3_4/Assets/Scripts/Hud.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Hud : MonoBehaviour
 {
+   //Instancia
    public static Hud instance;
+   //Sliders 
+   [Header("Sliders")]
    public StatsBar lifeBar;
    public StatsBar hungerBar;
    public StatsBar thirstBar;
    public StatsBar tempBar;
-
+   //txt e  transform
+   [Header("Tempo e Sol")]
+   [SerializeField] private TextMeshProUGUI timeTxt;
+   [SerializeField] public Transform directionalLight;
    void Awake(){
       instance = this;
    }
+   //Coloca os valores maximos e minimos do Status do player 
    public void ColocaStatMax(StatsBar stat, int val){
       stat.SetStatsMax(val);
    }
@@ -21,10 +30,9 @@ public class Hud : MonoBehaviour
       stat.SetTempMin(valMin);
       stat.UpdateStatsFloat(valIni);
    }
-   /*public void ColocaStatMinTemp(Barra_Status stat, float val){
-      stat.ColocarTempMinimo(val);
-   }*/
-   
+   public void ColocaStatMinTemp(StatsBar stat, float val){
+      stat.SetTempMin(val);
+   } 
    public void UpdateVidaHud(int vida){
         ColocaStatMax(lifeBar, vida);
    }
@@ -37,4 +45,24 @@ public class Hud : MonoBehaviour
    public void UpdateTempHud(float tempMax, float tempMin, float tempValue){
         ColocaStatTemp(tempBar, tempMax, tempMin, tempValue);
    }
+   //
+   public void updateLife(int life)
+    {
+        lifeBar.UpdateStats(life);
+    }
+   public void updateFood(int food){
+      hungerBar.UpdateStats(food);
+   }
+   public void updateWater(int water){
+       thirstBar.UpdateStats(water);
+   }
+   public void CalcTime(float seconds){
+       timeTxt.text = TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm");
+   }
+   public void prosCeu(float seconds)
+    {
+      float rotX = Mathf.Lerp(-90, 270, seconds/86400);
+      directionalLight.rotation = Quaternion.Euler(rotX,0,0);
+    }
+    
 }
