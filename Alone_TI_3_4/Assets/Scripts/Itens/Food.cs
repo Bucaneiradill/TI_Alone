2777bf5a85c6060293e532211eb64d78539285eb
Guilherme Cesário <guilhermecesario1808@gmail.com>
2023-06-03 19:55:19 -0300

    classes de itens separadas; problema no ícone dos itens

---
+++ Alone_TI_3_4/Assets/Scripts/Itens/Food.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Food")]
+public class Food : Item
+{
+    public override void PerformAction()
+    {
+        // Lógica específica para ação da comida
+    }
+}
