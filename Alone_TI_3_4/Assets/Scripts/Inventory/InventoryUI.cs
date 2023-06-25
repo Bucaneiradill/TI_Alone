ca30036ade3ea61e1a5a0b8fb2a76b5eb253c3ed
Italack <78178222+Italack@users.noreply.github.com>
2023-06-25 17:20:01 -0300

    comentarios dos inventario

--- Alone_TI_3_4/Assets/Scripts/Inventory/InventoryUI.cs
+++ Alone_TI_3_4/Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,3 +1,15 @@
+/**************************************************************
+    Jogos Digitais SG
+    InventoryUI
+
+    Descrição:  Gerencia a interface e adiciona os itens no InventorySlot.
+
+    Alone - Jogos Digitais SG –  09/04/2022
+    Modificado por: 
+***************************************************************/
+
+//-------------------------- Bibliotecas Usadas --------------------------------
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +22,12 @@ public class InventoryUI : MonoBehaviour
     public GameObject recipeUI;
     InventorySlot[] slots;
 
+    /*------------------------------------------------------------------------------
+    Função:     Awake
+    Descrição:  
+    Entrada:    -
+    Saída:      -
+    ------------------------------------------------------------------------------*/
     private void Awake()
     {
         if (instance == null)
@@ -21,18 +39,33 @@ public class InventoryUI : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+    /*------------------------------------------------------------------------------
+    Função:     Start
+    Descrição:  
+    Entrada:    -
+    Saída:      -
+    ------------------------------------------------------------------------------*/
     void Start(){
         Inventory.instance.onItemChangeCallBack += UpdateUI;
         slots = itemsParent.GetComponentsInChildren<InventorySlot>(); //Pega todos os slots que são filhos do InventorySlot.
     }
-
+    /*------------------------------------------------------------------------------
+    Função:     Update
+    Descrição:  Verifica quando o usuário abre o inventário
+    Entrada:    -
+    Saída:      -
+    ------------------------------------------------------------------------------*/
     void Update(){
         if(Input.GetButtonDown("Inventory")){
             inventoryUI.SetActive(!inventoryUI.activeSelf); //Inverte o estado atual do GameObject e inverte quando a tecla é apertada novamente.
         }
     }
-
+    /*------------------------------------------------------------------------------
+    Função:     UpdateUI
+    Descrição:  Atualiza a interface com base nos items da lista.
+    Entrada:    -
+    Saída:      -
+    ------------------------------------------------------------------------------*/
     void UpdateUI(){
         for(int i = 0; i < slots.Length;  i++){ //Loop para passando por todos os slots do inventário.
             if(i < Inventory.instance.items.Count){ //Confere se o quantos items tem no inventário.
