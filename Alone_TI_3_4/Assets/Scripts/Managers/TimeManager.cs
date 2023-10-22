43f3681a2ad7596e1367f17083a9f869fa7d0533
Guilherme Cesário <guilhermecesario1808@gmail.com>
2023-10-22 13:01:17 -0300

    tempo passando mais rápido; verificação da cura corrigida

--- Alone_TI_3_4/Assets/Scripts/Managers/TimeManager.cs
+++ Alone_TI_3_4/Assets/Scripts/Managers/TimeManager.cs
@@ -16,7 +16,7 @@ public class TimeManager : MonoBehaviour
     [SerializeField] public Transform directionalLight;
     [SerializeField] private int cont = 0;
     [SerializeField] private int cont2 = 0;
-    [SerializeField][Tooltip("Valor padrão do delay")] private float dalayValue = 0.1f;
+    [SerializeField][Tooltip("Valor padrão do delay")] private float dalayValue = 0.03f;
     public float delay;
     public string timeString;
     public bool isPlaying;
