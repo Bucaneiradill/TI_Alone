eb47a46a5392d6ae4cd5f0cdadf2af54d451bc8a
Enzo Rafael <enzoricarteaa@gmail.com>
2023-11-29 13:29:15 -0300

    Save player position

--- Alone_TI_3_4/Assets/Scripts/SaveGame/SaveGame.cs
+++ Alone_TI_3_4/Assets/Scripts/SaveGame/SaveGame.cs
@@ -4,6 +4,7 @@ using System.IO;
 class SceneData{
    public GameManagerData gameManager;
    public TimeManagerData timeManager;
+   public PlayerData playerData;
 }
 
 public class SaveGame : MonoBehaviour
@@ -23,19 +24,25 @@ public class SaveGame : MonoBehaviour
 
     void Save(){
         SceneData data = new SceneData();
+
         data.gameManager = GameManager.instance.GetGameManager();
         data.timeManager = TimeManager.instance.GetTimeManagerData();
+        PlayerActions player = FindObjectOfType<PlayerActions>();
+        data.playerData = new PlayerAdapter(player);
        
         string s = JsonUtility.ToJson(data, true);
         Debug.Log("S");
         File.WriteAllText(path, s);
     }
     void Load(){
-       
+        PlayerActions player = FindObjectOfType<PlayerActions>();
+        player.target = null;
+
         string s =File.ReadAllText(path);
         SceneData data = JsonUtility.FromJson<SceneData>(s);
         GameManager.instance.SetGameManagerData(data.gameManager);
         TimeManager.instance.SetTimeManagerData(data.timeManager);
-        
+        player.transform.position = data.playerData.position; 
+        player.transform.eulerAngles = data.playerData.rotation;
     }
 }
