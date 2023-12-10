e547b1247fd3d4a49fb0c8cfeed17475fab03dff
Guilherme Cesário <guilhermecesario1808@gmail.com>
2023-12-10 04:25:20 -0300

    área de efeito base da fogueira

--- Alone_TI_3_4/Assets/Scripts/Itens/Campfire.cs
+++ Alone_TI_3_4/Assets/Scripts/Itens/Campfire.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Campfire : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    bool turnedOn;
+    [SerializeField] float effectRadius = 4f;
+
+    public override void Interact()
     {
-        
+        base.Interact();
+        turnedOn = !turnedOn;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+        if (turnedOn)
+        {
+            // Verificar se o jogador está dentro da esfera
+            Collider[] colliders = Physics.OverlapSphere(transform.position, effectRadius);
+
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("Player"))
+                {
+                    // O jogador está dentro da área da fogueira
+                    Debug.Log("Player is inside campfire radius");
+                    // Faça o que precisar fazer quando o jogador estiver dentro da área
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, effectRadius);
     }
 }
